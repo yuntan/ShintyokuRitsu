88cc61201bf9865728e2a694f79143e73c78e047
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2014-02-25 09:36:16 +0900

    add func to Storage
    add readTasksByFolder

--- js/Storage.js
+++ js/Storage.js
@@ -1,10 +1,10 @@
-//.pragma library
+.pragma library
 .import QtQuick.LocalStorage 2.0 as Sql
 
 /*********************************************************
   id (int): Task ID
   name (str): Task name
-  isProject (bool): Task is part of another project or not
+  isProject (bool): The task can contain sub tasks of not.
   importance (int): 0, 1, 2, or 3. Higher priority higher
     number.
   deadline (date): Task deadline
@@ -12,10 +12,11 @@
   targetTime (int): Estimated time the task requires in min
   percent (int): Current task rate
   folder (str): folder the task belong to
-  projectId (int): If the isProject is true, the id of task
-    that the task belong to. Else 0.
+  projectId (int): If the task belongs to another project,
+    this value should be id of the project. Else -1.
 **********************************************************/
 
+// Storage class
 function Storage() {
 	console.debug("Creating database...")
 	this.db = Sql.LocalStorage.openDatabaseSync(
@@ -38,21 +39,23 @@ function Storage() {
 
 Storage.prototype = {
 
+    // Read all tasks its projectId matches with argument.
 	readTasks : function(projectId) {
 		console.debug("Reading tasks... (projectId: " + projectId + ")")
 		var r
 		this.db.transaction(function(tx) {
 			if(projectId === "" || projectId === undefined) {
-				r = tx.executeSql('SELECT * FROM Tasks WHERE projectId = -1')
+                r = tx.executeSql("SELECT * FROM Tasks WHERE projectId = -1")
 			}
 			else {
-				r = tx.executeSql('SELECT * FROM Tasks WHERE projectId = ?', projectId)
+                r = tx.executeSql("SELECT * FROM Tasks WHERE projectId = ?", projectId)
 			}
 		})
 		return r
 	},
 
-	readTask : function(id) {
+    // Returns a task its id is matches with argument.
+    readTaskById : function(id) {
 		console.debug("Reading task... (id: " + id + ")")
 		var r
 		this.db.transaction(function(tx) {
@@ -60,22 +63,37 @@ Storage.prototype = {
 				return
 			}
 			else {
-				r = tx.executeSql('SELECT * FROM Tasks WHERE id = ?', id)
+                r = tx.executeSql("SELECT * FROM Tasks WHERE id = ?", id)
 			}
 		})
 		return r
-	},
+    },
 
+    // Returns tasks its belonging folder is match with argument.
+    readTasksByFolder: function(folder) {
+        console.debug("Reading task... (folder: " + folder + ")")
+        var r
+        this.db.transaction(function(tx) {
+            if(id === undefined) {
+                return
+            }
+            else {
+                r = tx.executeSql("SELECT * FROM Tasks WHERE folder = '?'", folder)
+            }
+        })
+        return r
+    },
+
+    // Create a new task in database
     addTask : function(name, isProject, importance, deadline, detail, targetTime, percent, folder, projectId) {
 		console.debug("Adding Task...")
 		this.db.transaction(function(tx) {
 			var r = tx.executeSql(
-					'INSERT OR REPLACE INTO Tasks(name, isProject, importance, ' +
-                    'deadline, detail, targetTime, percent, folder, projectId) ' +
-                    'VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                    "INSERT OR REPLACE INTO Tasks(name, isProject, importance, " +
+                    "deadline, detail, targetTime, percent, folder, projectId) " +
+                    "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     [name, isProject, importance, deadline, detail, targetTime, percent, folder, projectId])
 			console.log("Task Saved: " + r)
 		})
 	}
 }
-
